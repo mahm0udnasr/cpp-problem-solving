afbd20fd2dd450e2fb491edc99136713eba9554f
mahm0udnasr <mahmoudnasrlite@gmail.com>
2025-06-11 08:46:05 +0300

    add problem number 21

---
+++ 21 - GenerateKeys.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int ReadPositiveNumber(string Msg) {
+    int number = 0;
+    do {
+        cout << Msg;
+        cin >> number;
+    } while (number <= 0);
+    return number;
+}
+
+char RandomCapitalChar() {
+    int randomNumber = rand() % (65 - 90 + 1) + 65;
+    return char(randomNumber);
+}
+
+string GenerateRandomKey() {
+    string Key;
+    for (int i = 1; i <= 4; i++) {
+        Key += RandomCapitalChar();
+    }
+    return Key;
+}
+
+void PrintRandomKeys(int length) {\
+    for (int i = 1; i <= length; i++) {
+        cout << "\n";
+        cout << "Key [";
+        if (i <= 9) {
+            cout << "0" << i;
+        }
+        else {
+            cout << i;
+        }
+        cout << "] : ";
+        for (int j = 1; j <= 4; j++) {
+            cout << GenerateRandomKey();
+            if (j <= 3) {
+                cout << "-";
+            }
+            else {
+                continue;
+            }
+        }
+        cout << "\n";
+    }
+}
+
+int main() {
+    srand((unsigned)time(NULL));
+    int length = ReadPositiveNumber("Entre A Length Of Keys: ");
+    PrintRandomKeys(length);
+	return 0;
+}
