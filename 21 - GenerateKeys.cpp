6540d865d823627d275fed2d84fb3469fd6fbd4f
mahm0udnasr <mahmoudnasrlite@gmail.com>
2025-06-11 09:01:09 +0300

    another solution for problem 21

--- 21 - GenerateKeys.cpp
+++ 21 - GenerateKeys.cpp
@@ -1,30 +1,79 @@
 #include <iostream>
 #include <cstdlib>
+#include <ctime>
 using namespace std;
+
+enum enCharType { SmallLetter = 1, CapitalLetter = 2, SpecialCharacter = 3, Digit = 4 };
+
 
 int ReadPositiveNumber(string Msg) {
     int number = 0;
     do {
         cout << Msg;
         cin >> number;
-    } while (number <= 0 || number >= 100);
+    } while (number <= 0);
     return number;
 }
 
-char GetRandomCapitalChar() {
-    return char(rand() % (65 - 90 + 1) + 65);
+int RandomNumber(int From, int To)
+{
+    int randNum = rand() % (To - From + 1) + From;
+    return randNum;
 }
 
-string GenerateRandomKey() {
+char GetRandomCharacter(enCharType CharType)
+{
+    switch (CharType)
+    {
+    case enCharType::SmallLetter:
+    {
+        return char(RandomNumber(97, 122));
+        break; // break is not necessary after return.
+    }
+    case enCharType::CapitalLetter:
+    {
+        return char(RandomNumber(65, 90));
+        break;
+    }
+    case enCharType::SpecialCharacter:
+    {
+        return char(RandomNumber(33, 47));
+        break;
+    }
+    case enCharType::Digit:
+    {
+        return char(RandomNumber(48, 57));
+        break;
+    }
+    }
+    return '\0';
+}
+
+string GenerateRandomKey(enCharType CharType, short Length) {
     string Key;
-    for (int i = 1; i <= 4; i++) {
-        Key += GetRandomCapitalChar();
+    for (int i = 1; i <= Length; i++) {
+        Key += GetRandomCharacter(CharType);
     }
     return Key;
 }
 
-void PrintRandomKeys(int length) {\
-    for (int i = 1; i <= length; i++) {
+string GenerateKey()
+{
+    string Key = "";
+    for (int j = 1; j <= 4; j++) {
+        Key += GenerateRandomKey(enCharType::CapitalLetter, 4);
+        if (j <= 3) {
+            Key += "-";
+        }
+        else {
+            continue;
+        }
+    }
+    return Key;
+}
+
+void GenerateKeys(short NumberOfKeys) {
+    for (int i = 1; i <= NumberOfKeys; i++) {
         cout << "\n";
         cout << "Key [";
         if (i <= 9) {
@@ -34,22 +83,13 @@ void PrintRandomKeys(int length) {\
             cout << i;
         }
         cout << "] : ";
-        for (int j = 1; j <= 4; j++) {
-            cout << GenerateRandomKey();
-            if (j <= 3) {
-                cout << "-";
-            }
-            else {
-                continue;
-            }
-        }
-        cout << "\n";
+        cout << GenerateKey() << endl;
     }
 }
 
 int main() {
     srand((unsigned)time(NULL));
-    int length = ReadPositiveNumber("Entre A Length Of Keys: ");
-    PrintRandomKeys(length);
-	return 0;
+    short NumberOfKeys = ReadPositiveNumber("Entre A Length Of Keys: ");
+    GenerateKeys(NumberOfKeys);
+    return 0;
 }
