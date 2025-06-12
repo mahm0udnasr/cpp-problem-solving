888a2fdd291b6b92ee88496156a9dc8282985cbd
mahm0udnasr <mahmoudnasrlite@gmail.com>
2025-06-12 07:32:53 +0300

    add problem 23

---
+++ 23 - FillArrayWithRandomNumberFrom1To100.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int ReadPositiveNumber(string Msg) {
+    int number = 0;
+    do {
+        cout << Msg;
+        cin >> number;
+    } while (number <= 0);
+    return number;
+}
+
+int RandomNumber(int From, int To) {
+    return rand() % (To - From + 1) + From;
+}
+
+void FillArrayWithRandomNumbers(int arr[100], int& ArrayLength) {
+    ArrayLength = ReadPositiveNumber("Enter Array Length: ");
+    for (int i = 0; i <= ArrayLength - 1; i++) {
+        arr[i] = RandomNumber(1, 100);
+    }
+}
+
+void PrintArray(int arr[100], int ArrayLength) {
+    for (int i = 0; i <= ArrayLength - 1; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+}
+
+int main() {
+    srand((unsigned)time(NULL));
+    int arr[100], ArrayLength;
+    FillArrayWithRandomNumbers(arr, ArrayLength);
+    cout << "\nArray Elements: ";
+    PrintArray(arr, ArrayLength);
+    return 0;
+}
