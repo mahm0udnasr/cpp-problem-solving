2901852fe67fe59cfe5c58cd311b51a1116193c7
mahm0udnasr <mahmoudnasrlite@gmail.com>
2025-06-17 23:56:04 +0300

    another solution in 36

--- 36 - AddArrayElementSimiDynamic.cpp
+++ 36 - AddArrayElementSimiDynamic.cpp
@@ -17,25 +17,31 @@ void PrintArray(int arr[100], int arrLength) {
     cout << "\n";
 }
 
+void AddArrayElement(int Number, int arr[100], int& arrLength) {
+  arrLength++;
+  arr[arrLength - 1] = Number;
+}
+
 void InputUserNumberInArray(int arr[100], int& arrLength) {
     bool AddMoreNumber = false;
     do
     {
-        int Element = ReadPositiveNumber("Please Enter A Number: ");
-        for (int i = 0; i <= arrLength; i++) {
-            arr[arrLength] = Element;
-        }
+        // int Element = ReadPositiveNumber("Please Enter A Number: ");
+        // for (int i = 0; i <= arrLength; i++) {
+        //     arr[arrLength] = Element;
+        // }
+        AddArrayElement(ReadPositiveNumber("Please Enter A Number: "), arr, arrLength);
         cout << "Do You Want To Add More Number [0, 1]: ";
         cin >> AddMoreNumber;
-        arrLength += 1;
-    } while (AddMoreNumber == true);
+        // arrLength += 1;
+    } while (AddMoreNumber);
 }
 
 
 int main() {
     int arr[100], arrLength = 0;
     InputUserNumberInArray(arr, arrLength);
-    cout << "Array Length: " << arrLength << endl;
+    cout << "\nArray Length: " << arrLength << endl;
     cout << "Array 1 elements: ";
     PrintArray(arr, arrLength);
     return 0;
